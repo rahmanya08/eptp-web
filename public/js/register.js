45ac84e85f871f90aa6192ce86b1418cdee6b137
Ahmed Hegazy <ahmedhegazee@hotmail.com>
2023-01-30 11:36:34 +0200

    solve role select problem

--- public/js/register.js
+++ public/js/register.js
@@ -1,167 +1,144 @@
-const btnNext = document.querySelector('form .btn .next');
-const btnBack = document.querySelector('form .btn .back');
-const indicator = document.querySelector('.indicator .line span');
-const indicatorItems = document.querySelectorAll('.indicator p');
-const form = document.querySelector('form');
-const allTab = document.querySelectorAll('form .tab');
+const btnNext = document.querySelector("form .btn .next");
+const btnBack = document.querySelector("form .btn .back");
+const indicator = document.querySelector(".indicator .line span");
+const indicatorItems = document.querySelectorAll(".indicator p");
+const form = document.querySelector("form");
+const allTab = document.querySelectorAll("form .tab");
 const filebtn = document.getElementById("file");
 const customBtn = document.getElementById("upload-btn");
 const customTxt = document.getElementById("custom-msg");
-const imgArea = document.querySelector('.img-area');
-var checkBox = document.getElementById('accept');
+const imgArea = document.querySelector(".img-area");
+var checkBox = document.getElementById("accept");
 let i = 0;
 
-
-allTab[i].classList.add('show');
+allTab[i].classList.add("show");
 indicator.style.width = i;
-indicatorItems[i].classList.add('active');
+indicatorItems[i].classList.add("active");
 
-if(i === 0) {
-    btnBack.style.display = 'none';
-}
-else
-{
-    btnBack.style.display = 'block';
+if (i === 0) {
+    btnBack.style.display = "none";
+} else {
+    btnBack.style.display = "block";
 }
 
-btnNext.addEventListener('click', function() {
-    const allInputPerTab = allTab[i].querySelectorAll('input');
-    for(let j = 0; j < allInputPerTab.length; j++) {
-        allInputPerTab[j].addEventListener('input', function() {
-
-            allInputPerTab[j].style.borderColor = '#ddd';
-
+btnNext.addEventListener("click", function () {
+    const allInputPerTab = allTab[i].querySelectorAll("input");
+    for (let j = 0; j < allInputPerTab.length; j++) {
+        allInputPerTab[j].addEventListener("input", function () {
+            allInputPerTab[j].style.borderColor = "#ddd";
         });
-        
-        
-        if(allInputPerTab[j].value === '' || !allInputPerTab[j].checkValidity()) {
-            allInputPerTab[j].style.borderColor = 'red';
-            return false
-        }
-        else
-        {
-            allInputPerTab[j].style.borderColor = '#ddd';
+
+        if (
+            allInputPerTab[j].value === "" ||
+            !allInputPerTab[j].checkValidity()
+        ) {
+            allInputPerTab[j].style.borderColor = "red";
+            return false;
+        } else {
+            allInputPerTab[j].style.borderColor = "#ddd";
         }
     }
 
     i += 1;
 
-    if(i >= allTab.length) {
+    if (i >= allTab.length) {
         form.submit();
         return false;
-    } 
-    else 
-    {
-        for(let j = 0; j < allTab.length; j++) {
-            allTab[j].classList.remove('show');
-            indicatorItems[j].classList.remove('active');
+    } else {
+        for (let j = 0; j < allTab.length; j++) {
+            allTab[j].classList.remove("show");
+            indicatorItems[j].classList.remove("active");
         }
 
-        for(let j = 0; j < i; j++) {
-            indicatorItems[j].classList.add('active');
+        for (let j = 0; j < i; j++) {
+            indicatorItems[j].classList.add("active");
         }
 
-        allTab[i].classList.add('show');
-        indicator.style.width = '${i * 50}%';
-        indicatorItems[i].classList.add('active');
-    }   
-    if(i === 0) {
-        btnBack.style.display = 'none';
+        allTab[i].classList.add("show");
+        indicator.style.width = "${i * 50}%";
+        indicatorItems[i].classList.add("active");
     }
-    else
-    {
-        btnBack.style.display = 'block';
+    if (i === 0) {
+        btnBack.style.display = "none";
+    } else {
+        btnBack.style.display = "block";
     }
 
-    if(i === allTab.length - 1) {
-        btnNext.innerHTML = 'Submit';
+    if (i === allTab.length - 1) {
+        btnNext.innerHTML = "Submit";
+    } else {
+        btnNext.innerHTML = "Next";
     }
-    else
-    {
-        btnNext.innerHTML = 'Next';
-    }
-
-
 });
 
-btnBack.addEventListener('click', function () {
-    
+btnBack.addEventListener("click", function () {
     i -= 1;
 
-   
-    for(let j = 0; j < allTab.length; j++) {
-        allTab[j].classList.remove('show');
-        indicatorItems[j].classList.remove('active');
+    for (let j = 0; j < allTab.length; j++) {
+        allTab[j].classList.remove("show");
+        indicatorItems[j].classList.remove("active");
     }
 
-    for(let j = 0; j < i; j++) {
-        indicatorItems[j].classList.add('active');
+    for (let j = 0; j < i; j++) {
+        indicatorItems[j].classList.add("active");
     }
 
-    allTab[i].classList.add('show');
-    indicator.style.width = '${i * 50}%';
-    indicatorItems[i].classList.add('active');
+    allTab[i].classList.add("show");
+    indicator.style.width = "${i * 50}%";
+    indicatorItems[i].classList.add("active");
 
-    if(i === 0) {
-        btnBack.style.display = 'none';
-    }
-    else
-    {
-        btnBack.style.display = 'block';
+    if (i === 0) {
+        btnBack.style.display = "none";
+    } else {
+        btnBack.style.display = "block";
     }
 
-    if(i === allTab.length - 1) {
-        btnNext.innerHTML = 'Submit';
+    if (i === allTab.length - 1) {
+        btnNext.innerHTML = "Submit";
+    } else {
+        btnNext.innerHTML = "Next";
     }
-    else
-    {
-        btnNext.innerHTML = 'Next';
-    }
-    
 });
 
-customBtn.addEventListener("click", function(){
+customBtn.addEventListener("click", function () {
     filebtn.click();
 });
 
-filebtn.addEventListener("change",function(){
-    if(filebtn.value) {
-        
-        const image = this.files[0]
+filebtn.addEventListener("change", function () {
+    if (filebtn.value) {
+        const image = this.files[0];
         console.log(image);
         const reader = new FileReader();
-        reader.onload = ()=> {
+        reader.onload = () => {
             const imgUrl = reader.result;
-            const img = document.createElement('img');
+            const img = document.createElement("img");
             img.src = imgUrl;
             imgArea.appendChild(img);
-        }
+        };
         reader.readAsDataURL(image);
 
-        
-        
         customTxt.innerHTML = filebtn.value.match(
             /[\/\\]([\w\d\s\.\-\(\)]+)$/
-        )[1]
-
-    }
-    else
-    {
+        )[1];
+    } else {
         customTxt.innerHTML = "No File Chosen";
     }
-})
+});
 
-checkBox.onchange = function(){
-    if(this.checked){
-        
-        btnNext.innerHTML = 'Submit';
+checkBox.onchange = function () {
+    if (this.checked) {
+        btnNext.innerHTML = "Submit";
         btnNext.disabled = false;
-    }
-    else
-    {
-        btnNext.innerHTML = 'Submit';
+    } else {
+        btnNext.innerHTML = "Submit";
         btnNext.disabled = true;
     }
-}
+};
 
-
+const roleSelect = document.querySelector("#roleSelect");
+const studentFields = document.querySelectorAll(".student-fields");
+roleSelect.addEventListener("change", function () {
+    if (roleSelect.value === "4")
+        studentFields.forEach((el) => el.classList.remove("hidden"));
+    else studentFields.forEach((el) => el.classList.add("hidden"));
+});
--- resources/views/test-register.blade.php
+++ resources/views/test-register.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.master-form')
 
 @section('title', 'Registration')
@@ -8,74 +7,77 @@
 @endpush
 
 @section('container')
-        <div class="indicator">
-            <span class="line"><span></span></span>
-            <p class="active">1</p>
-            <p>2</p>
-            <p>3</p>
-            <p>4</p>
-        </div>
-        @csrf
-        <form action="#">
-            <div class="tab show">
-                <h1>Profile Infromation</h1>
-                <p>fill out a few details to start registration the test</p>
-                <div class="container-upload">
-                    <div class="img-area">
-                        <i class='bx bxs-cloud-upload icon'></i>
-                    </div> 
-                    <div class="upload">
-                        <input type="file" hidden="hidden" name="file" id="file" accept="image/jpg,image/png,image/jpeg" />
-                        <span id="custom-msg">No File chosen, yet.</span>
-                        <button type="button" class="btn-hover" id="upload-btn"  >Browse</button>
-                    </div>
+    <div class="indicator">
+        <span class="line"><span></span></span>
+        <p class="active">1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>4</p>
+    </div>
+    @csrf
+    <form action="#">
+        <div class="tab show">
+            <h1>Profile Infromation</h1>
+            <p>fill out a few details to start registration the test</p>
+            <div class="container-upload">
+                <div class="img-area">
+                    <i class='bx bxs-cloud-upload icon'></i>
                 </div>
-                <div class="form">
-                    <label for="name" class="form-label">Full Name</label>
-                    <input type="text" name="fullname" placeholder="Full Name">
+                <div class="upload">
+                    <input type="file" hidden="hidden" name="file" id="file"
+                        accept="image/jpg,image/png,image/jpeg" />
+                    <span id="custom-msg">No File chosen, yet.</span>
+                    <button type="button" class="btn-hover" id="upload-btn">Browse</button>
                 </div>
-                <div class="row">
-                    <div class="col">
-                      <label for="gender" class="form-label">Gender</label><br>
+            </div>
+            <div class="form">
+                <label for="name" class="form-label">Full Name</label>
+                <input type="text" name="fullname" placeholder="Full Name">
+            </div>
+            <div class="row">
+                <div class="col">
+                    <label for="gender" class="form-label">Gender</label><br>
                     <div class="mt-1">
-                      <input class="form-check-input" type="radio" name="malerad" id="inlineRadio1" value="male">
-                      <label class="form-check-label" for="inlineRadio1">Male</label>
-                      <input class="form-check-input" type="radio" name="femalerad" id="inlineRadio2" value="female">
-                      <label class="form-check-label" for="inlineRadio2">Female</label> </div>  
-                    </div>
-                    <div class="col-form">
-                      <label for="birth" class="form-label">Date of Birth</label>
-                      <input type="date" class="form-control" placeholder="Last name" aria-label="Last name">
+                        <input class="form-check-input" type="radio" name="malerad" id="inlineRadio1" value="male">
+                        <label class="form-check-label" for="inlineRadio1">Male</label>
+                        <input class="form-check-input" type="radio" name="femalerad" id="inlineRadio2" value="female">
+                        <label class="form-check-label" for="inlineRadio2">Female</label>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col">
+                <div class="col-form">
+                    <label for="birth" class="form-label">Date of Birth</label>
+                    <input type="date" class="form-control" placeholder="Last name" aria-label="Last name">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
                     <label for="identity" class="form-label">Identity Type</label><br>
                     <div class="mt-1">
                         <input class="form-check-input" type="radio" name="ktp" id="inlineRadio1" value="ktp">
                         <label class="form-check-label" for="inlineRadio1">KTP</label>
                         <input class="form-check-input" type="radio" name="ktm" id="inlineRadio2" value="ktm">
-                        <label class="form-check-label" for="inlineRadio2">KTM</label> </div>
-                    </div>
-                    <div class="col-form">
-                    <label for="inumber" class="form-label">Identity Number</label>
-                    <input type="text" class="form-control" name="inumber" placeholder="Number of Identity">
+                        <label class="form-check-label" for="inlineRadio2">KTM</label>
                     </div>
                 </div>
+                <div class="col-form">
+                    <label for="inumber" class="form-label">Identity Number</label>
+                    <input type="text" class="form-control" name="inumber" placeholder="Number of Identity">
+                </div>
+            </div>
             <div class="form">
                 <label for="status" class="form-label">Participant Status</label>
                 <div class="row-select">
                     <div class="col-select">
-                        <select class="form-select" aria-label="Default select example">
+                        <select class="form-select" id="roleSelect" aria-label="Default select example">
                             <option selected>Select Your Status</option>
                             <option value="1">Public</option>
                             <option value="2">Lecturer</option>
                             <option value="3">Employee/Staff</option>
-                            <option value="3">Student</option>
+                            <option value="4">Student</option>
                         </select>
                     </div>
-                    <div class="col-select">
-                        <select  class="form-select" aria-label="Default select example">
+                    <div class="col-select hidden student-fields">
+                        <select class="form-select" aria-label="Default select example">
                             <option selected>Study Program</option>
                             <option value="1">D3-TI</option>
                             <option value="2">D3-TM</option>
@@ -83,7 +85,7 @@
                             <option value="3">D4-TPPL</option>
                         </select>
                     </div>
-                    <div class="col-select">
+                    <div class="col-select hidden student-fields">
                         <select class="form-select" aria-label="Default select example">
                             <option selected>Major</option>
                             <option value="1">T. Infromatika</option>
@@ -92,8 +94,8 @@
                             <option value="3">T. Mesin</option>
                         </select>
                     </div>
-                    <div class="col-select">
-                        <select  class="form-select" aria-label="Default select example">
+                    <div class="col-select hidden student-fields">
+                        <select class="form-select" aria-label="Default select example">
                             <option selected>Semester</option>
                             <option value="1">1</option>
                             <option value="2">2</option>
@@ -102,7 +104,7 @@
                         </select>
                     </div>
                 </div>
-                
+
                 <h1>Contact</h1>
                 <div class="form">
                     <label for="address" class="form-label">Address</label>
@@ -141,16 +143,16 @@
             </div>
             <div class="tab view">
                 <h1>Test Regulation</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                    Aliquam non minus laudantium quae. 
-                    Cupiditate et reiciendis libero officia quia, 
-                    rem quo modi dolorem totam officiis labore, 
-                    tempore perspiciatis quas aspernatur. Lorem, 
-                    ipsum dolor sit amet consectetur adipisicing elit. 
-                    Perspiciatis reiciendis inventore odio maiores dolorem quos possimus, 
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Aliquam non minus laudantium quae.
+                    Cupiditate et reiciendis libero officia quia,
+                    rem quo modi dolorem totam officiis labore,
+                    tempore perspiciatis quas aspernatur. Lorem,
+                    ipsum dolor sit amet consectetur adipisicing elit.
+                    Perspiciatis reiciendis inventore odio maiores dolorem quos possimus,
                     aperiam aspernatur illo rem, itaque dolores ipsum labore eaque debitis,
                     tenetur facere assumenda quam?
-                </p> 
+                </p>
                 <div class="test-reg">
                     <input type="checkbox" name="accept" id="accept">
                     <label for="check-label">I'll accept the regulation</label>
@@ -161,10 +163,9 @@
                 <button type="button" class="back">Back</button>
                 <button type="button" class="next">Next</button>
             </div>
-        </form>  
-    @endsection
+    </form>
+@endsection
 
 @push('child-js')
     <script src="{{ asset('js/register.js') }}"></script>
 @endpush
-
